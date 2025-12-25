5529a74872112cab6cdcdb3da8ba66c31e1bc561
kavin raja <kavinkr10@gmail.com>
2025-12-25 21:06:57 +0530

    re-arrange files

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM golang:1.24-alpine AS builder
 WORKDIR /app
 COPY . .
 RUN go mod download
-RUN go build -o main .
+RUN go build -o main ./api/main.go
 
 FROM alpine:latest
 WORKDIR /root/
--- sdk/termstat.go
+++ sdk/termstat.go
@@ -4,6 +4,9 @@ import (
 	"os"
 	"runtime"
 	"time"
+	"encoding/json"
+	"fmt"
+	"os/exec"
 )
 
 type Event struct {
@@ -32,4 +35,13 @@ func Track(apiKey, version, cmd string) func(init){
 		}
 		sendAsync(event)
 	}
+}
+
+func sendAsync(event Event) {
+		data, _ := json.Marshal(event)
+		apiURL := "https://termstat-api-production.up.railway.app/v1/ping"
+		script := fmt.Sprintf(`curl -s -X POST %s -H "Content-Type: application/json" -d '%s' > /dev/null 2>&1 &`, apiURL, string(data))
+
+		cmd := exec.Command("sh", "-c", script)
+		_ = cmd.Start()
 }
